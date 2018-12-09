917e708355aab200d99b408385863a333cc474ba
johnmara72 <johnmara@admin.teithe.gr>
2018-12-10 00:27:47 +0200

    Καθαρισμός κώδικα

--- reset-password.php
+++ reset-password.php
@@ -99,19 +99,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
                 <label>Νέος Κωδικός:</label>
                 <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
-<!--                <span class="help-block"><?php echo $new_password_err; ?></span> -->
             </div>
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                 <label>Επιβεβαίωση Κωδικού:</label>
                 <input type="password" name="confirm_password" class="form-control">
-<!--                <span class="help-block"><?php echo $confirm_password_err; ?></span> -->
             </div>
-            
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <a class="btn btn-link" href="welcome.php">Cancel</a>
             </div>
-            
         </form>
     </div>  
     
